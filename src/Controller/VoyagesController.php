affbc8f5cd35fb97e3d77e4bb778b4b909df4247
Lenovo <Lenovo@DESKTOP-21LTUAB>
2024-12-10 16:45:03 +0100

    Phase 20 : sécurité.

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -48,11 +48,14 @@ public function __construct(VisiteRepository $repository) {
     }
      #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal')]
     public function findAllEqual($champ, Request $request): Response{
+         if($this->isCsrfTokenValid('filtre_'.$champ, $request->get('_token'))){
          $valeur = $request->get("recherche");
         $visites = $this->repository->findByEqualValue($champ,$valeur);
         return $this->render ("pages/voyages.html.twig",[
             'visites' => $visites
         ]);
+    }
+    return $this->redirectToRoute("voyages");
     }
     
     #[Route('/voyages/voyage/{id}', name: 'voyages.showone')]
